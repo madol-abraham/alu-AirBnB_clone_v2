7175fc14416b20eab833dff7484ebf73d5390a66
madol-abraham <m.madol@alustudent.com>
2024-02-04 05:14:17 -0800

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,38 @@
-#!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
-from datetime import datetime
-from fabric.api import local
+import unittest
+import MySQLdb
 
+class TestDatabaseFunctionality(unittest.TestCase):
+    def setUp(self):
+        # Connect to the test database
+        self.conn = MySQLdb.connect(
+            user='hbnb_test',
+            password='hbnb_test_pwd',
+            host='localhost',
+            database='hbnb_test_db'
+        )
+        self.cursor = self.conn.cursor()
 
-def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
+    def tearDown(self):
+        # Close the database connection
+        self.cursor.close()
+        self.conn.close()
+
+    def test_create_state(self):
+        # Get the initial number of records in the states table
+        self.cursor.execute("SELECT COUNT(*) FROM states")
+        initial_count = self.cursor.fetchone()[0]
+
+        # Execute the action (e.g., create a new state)
+        # Assume that you have a function to create a state in your application
+        create_state_function()  # Replace with the actual function call
+
+        # Get the number of records in the states table again
+        self.cursor.execute("SELECT COUNT(*) FROM states")
+        final_count = self.cursor.fetchone()[0]
+
+        # Validate the action
+        self.assertEqual(final_count, initial_count + 1, "New state was not created")
+
+if __name__ == '__main__':
+    unittest.main()
+
