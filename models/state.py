cd53c4da769872edaa76d2e0ed2dea54f5156453
madol-abraham <m.madol@alustudent.com>
2024-02-05 14:51:00 -0800

    committed

--- models/state.py
+++ models/state.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from os import getenv
-from sqlalchemy import String, DateTime, Column, ForeignKey
-from sqlalchemy.orm import relationship
-import models
+""" State Module for HBNB project """
+import os
+
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.city import City
+import models
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    _tablename_ = 'states'
+    """ State class """
+    __tablename__ = 'states'
+
     name = Column(String(128), nullable=False)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
         cities = relationship('City', backref='state',
                               cascade='all, delete-orphan')
+
     else:
+        # if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        # @property
+        # def cities(self):
+        #     """ Returns the list of City instances with state_id
+        #     equals to the current State.id. """
+        #     all_cities = list(models.storage.all(City).values())
+        #     return list(filter(lambda city: (city.id == self.id),
+        #     all_cities))
+        # no time to fix
         @property
         def cities(self):
-            """Getter attribute in case of file storage"""
+            """ Returns the list of City instances with state_id
+            equals to the current State.id. """
             return [city for city in models.storage.all(City).values()
                     if city.state_id == self.id]
